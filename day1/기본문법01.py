e503650ff5931bf94098b6d4120d45d375381a98
JaehonxKIM <wpmpqp1@naver.com>
2022-01-14 17:19:16 +0900

    파이썬 1일차 학습

---
+++ day1/기본문법01.py
@@ -0,0 +1,37 @@
+#주석입니다
+#문자열 표현방법 print() 활용
+print('Hello, world')
+print("Hello, world")
+
+#숫자연산 출력  print() 활용
+print(3 + 4)
+
+# 변수사용
+a = '안녕하세요'
+b = '반갑습니다!!'
+print(a)
+print(b)
+print(a,b,'정말?')
+
+#수학연산 / 정확히 소숫점으로 계산  // 정수로 나머지 계산 % 나머지 값 ** 거듭제곱
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/3)
+print(5//3)
+print(5%3)
+print(2**10)
+#int 나머지를 정수로 나옴
+print(int(5/3))
+
+
+
+print(float(5)) #정수 소수점 변환
+
+#TYPE 확인
+print(type(10))
+print(type(10.0))
+
+#괄호 (연산자 우선순위)
+print( (2+3)*5)
+print (2+ (3*5) )
